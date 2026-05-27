6d194c50ddc451334887216467b8d10c624adddf
hsieh c. yicheng <sinnoken@live.com>
2026-05-27 15:01:00 +0800

    Update topology.js with new export timestamp and positions

--- topology.js
+++ topology.js
@@ -1,4 +1,4 @@
-// Exported from BlastRadius POC at 2026-05-27T06:59:05.210Z
+// Exported from BlastRadius POC at 2026-05-27T07:00:37.642Z
 const topology = {
   nodes: [
     { id: "TPE", label: "TPE\nTW", type: "router", area: "0", stubs: ["1.1.1.1/32","10.1.0.0/24","100.64.0.0/24"], isASBR: true, isABR: false },
@@ -31,22 +31,22 @@ const topology = {
     { id: "e_FRA_PN", source: "FRA", target: "PN_EU", cost: 5, type: "transit" },
     { id: "e_AMS_PN", source: "AMS", target: "PN_EU", cost: 5, type: "transit" },
     { id: "e_LHR_FRA", source: "LHR", target: "FRA", cost: 10, type: "p2p" },
-    { id: "e_SIN_FRA_lease", source: "SIN", target: "FRA", cost: 80, type: "p2p" }
+    { id: "e_SIN_FRA_lease", source: "SIN", target: "FRA", cost: 80, type: "p2p", costRev: 70 }
   ],
   externals: [
     { advertising_router: "TPE", subnet: "0.0.0.0/0", metric: 1, metric_type: "E2" }
   ],
   positions: {
-    ICN: { x: 289, y: 15 },
-    TYO: { x: 380, y: 130 },
+    ICN: { x: 290, y: 32 },
+    TYO: { x: 366, y: 176 },
     TPE: { x: 126, y: 111 },
-    HKG: { x: 129, y: 285 },
-    SIN: { x: 297, y: 412 },
-    SYD: { x: 453, y: 279 },
-    LAX: { x: 572, y: 134 },
-    LHR: { x: 753, y: 201 },
-    AMS: { x: 754, y: 42 },
-    FRA: { x: 754, y: 412 },
-    PN_EU: { x: 887, y: 194 }
+    HKG: { x: 196, y: 263 },
+    SIN: { x: 266, y: 404 },
+    SYD: { x: 436, y: 312 },
+    LAX: { x: 601, y: 180 },
+    LHR: { x: 757, y: 184 },
+    AMS: { x: 758, y: 77 },
+    FRA: { x: 768, y: 407 },
+    PN_EU: { x: 894, y: 191 }
   },
 };
